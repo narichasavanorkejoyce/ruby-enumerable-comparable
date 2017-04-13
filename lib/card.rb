b25cb84ccd5c0a5996c839130647d0af92440fee
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-13 16:05:25 -0400

    Training notes

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 # A simple representation of a playing card.
+# We are writing a custom class
 class Card
   SUITS = %w[C D H S].freeze
   RANKS = [(2..10).to_a, %w[J Q K A]].flatten.freeze
 
   attr_reader :suit, :rank
+  include Comparable
+
+  # <=> here is basically defining a method
+  def <=>(other)
+    # then, we're going to compare the index numbers
+    suit_comparison = SUITS.index(suit) <=> SUITS.index(other.suit)
+    rank_comparison = RANKS.index(rank) <=> RANKS.index(other.rank)
+  end
+
+  if suit_comparison.zero?
+    rank_comparison
+  else
+    suit_comparison
+  end
 
   def initialize(rank, suit)
     unless SUITS.include? suit
--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -2,11 +2,27 @@
 
 #
 class SteppedRange
+  include Enumerable
   attr_reader :start, :stop, :step
+  # code outside this class cannot access start, stop, step
   private :start, :stop, :step
+  # this uniquely defines the stepped range
   def initialize(start, stop, step)
     @start = start
     @stop = stop
     @step = step
   end
+
+  # we are going to provide a block of code that will be applied to each item
+  def each
+    current = start
+    while current <= stop
+      # yield says, "hey, what's the value of the current value?"
+      # Kind of like return
+      yield current
+      current += step
+    end
+  end
 end
+
+# myRange = SteppedRange.new(1, 10, 3).to_a --> [1, 4, 7, 10]
